e729b18959ffa618576bd54c48f03155add03393
Dragos Tache <dragos.tache@interdigital.ro>
2023-06-07 21:36:44 +0300

    add vue project

---
+++ vueProject/hello-world/src/components/PrimaComp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="redBackground">
+    <h1>Prima</h1>
+    <p>{{ text }}</p>
+    <p></p>
+    <div>
+      {{ nickname }}
+    </div>
+    <div>5 + 18 = {{ 5 + 18 }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PrimaComp",
+  props: {
+    nickname: String,
+  },
+  data() {
+    return { text: "Prima" };
+  },
+};
+</script>
+<style>
+.redBackground {
+  background-color: red;
+}
+</style>
